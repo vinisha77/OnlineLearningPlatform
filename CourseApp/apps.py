20a81027de3aaafc58527ab5e3120cc054a4fd0b
unknown <chaitalideore555@gmail.com>
2024-05-21 13:38:09 +0200

    Formed Project Structure

--- CourseApp/apps.py
+++ CourseApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LearningappConfig(AppConfig):
+class CourseappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LearningApp'
+    name = 'CourseApp'
